5e85ca6bb73c37e5595132f2d4f09c3de987a597
Henrique Diniz <henriquee.diniz@hotmail.com>
2019-02-17 23:24:42 -0300

    Implemented socket.io to response real time

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect(
   "mongodb+srv://admin:admin123@social-app-2lvf1.mongodb.net/test?retryWrites=true",
@@ -10,10 +14,17 @@ mongoose.connect(
 }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+  
+  return next();
+});
+
 app.use(express.json());
 app.use(require('./routes'))
+app.use(cors());
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log(":) Server started on port 3000");
 });
 
