5cb556cb817460d7185515d89db720ef95284c4e
Henrique Diniz <henriquee.diniz@hotmail.com>
2019-02-19 13:33:42 -0300

    change order of cors call

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,9 +20,9 @@ app.use((req, res, next) => {
   return next();
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes'))
-app.use(cors());
 
 server.listen(3000, () => {
   console.log(":) Server started on port 3000");
