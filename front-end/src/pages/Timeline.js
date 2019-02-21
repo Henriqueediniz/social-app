92ce670e18d9e1ea04b74d2053c0e2865eba6c17
Henrique Diniz <henriquee.diniz@hotmail.com>
2019-02-21 20:00:32 -0300

    Start of implement from front-end of my twiiter app

---
+++ front-end/src/pages/Timeline.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class TimeLine extends Component {
+  render() {
+    return <h1>Timeline</h1>;
+  }
+}
