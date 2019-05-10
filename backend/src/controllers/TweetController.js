ed2842465edb8bf21bdcfebaa1ae2ee51f5b50c8
Henrique Diniz <henriquee.diniz@hotmail.com>
2019-05-10 00:06:34 -0300

    repair error on express

--- backend/src/controllers/TweetController.js
+++ backend/src/controllers/TweetController.js
@@ -1,28 +1,26 @@
-const Tweet = require('../models/Tweet');
+const Tweet = require("../models/Tweet");
 
 module.exports = {
-    async index(req, res) {
-        const tweets = await Tweet.find({}).sort('-createAt');
+  async index(req, res) {
+    const tweets = await Tweet.find({}).sort("-createAt");
 
-        return res.json(tweets);
-    },
+    return res.json(tweets);
+  },
 
-    async store(req, res) {
-        const tweet = await Tweet.create(req.body);
+  async store(req, res) {
+    const tweet = await Tweet.create(req.body);
 
-        req.io.emit('tweet', tweet);
+    req.io.emit("tweet", tweet);
 
-        return res.json(tweet);
-    },
+    return res.json(tweet);
+  },
 
-    async delete(req, res) {        
-         await Tweet.findByIdAndRemove(req.params.id);
-         const tweets = await Tweet.find({}).sort('-createAt');
+  async delete(req, res) {
+    await Tweet.findByIdAndRemove(req.params.id);
+    const tweets = await Tweet.find({}).sort("-createAt");
 
-         req.io.emit('delete',tweets);
+    req.io.emit("delete", tweets);
 
-         return res.send({tweets});
-         
-     }
- };
- 
+    return res.send({ tweets });
+  }
+};
